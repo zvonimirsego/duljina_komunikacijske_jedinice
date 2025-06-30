bcae67421d883af7d8e71eeaf1b70a697614cdb2
Zvonimir Šego <95874092+zvonimirsego@users.noreply.github.com>
2025-06-30 13:29:17 +0200

    Added .py file

---
+++ duljina_komunikacijske_jedinice.py
@@ -0,0 +1,25 @@
+# program koji ispisuje i vraća najdulju komunikacijsku jedinicu koje je dijete izgovorilo
+import os
+
+# ako netko ovo isproba samostalno, mora staviti path na put do vlastite mape u računalu
+path = r"UPISITE VAU PUTANJU OVDJE"
+
+def broj_rijeci(tekst):
+    return len(tekst.strip().split())
+
+for filename in os.listdir(path):
+    if filename.endswith(".cha"):
+        file_path = os.path.join(path, filename)
+        with open(file_path, 'r', encoding='utf-8') as tekst:
+            kom_jedinice = []
+            najduza = ""
+            najvise_rijeci = 0
+            for line in tekst:
+                if line.startswith("*CHI:"):
+                    jedinica = line[5:].strip()
+                    kom_jedinice.append(jedinica)
+                    br_rijeci = broj_rijeci(jedinica)
+                    if br_rijeci > najvise_rijeci:
+                        najvise_rijeci = br_rijeci
+                        najduza = jedinica
+            print(f"{filename}, {najduza}")
